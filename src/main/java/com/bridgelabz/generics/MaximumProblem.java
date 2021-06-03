ad882e0fc9c3f3a159efb76b9a8126de4a43e0f4
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-03 20:06:03 +0530

    Add-Three Float to Find Maximum

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -9,6 +9,15 @@ public class MaximumProblem {
             max =z;
         return max;
     }
+    public static Float maximumNumber(Float x,Float y, Float z){
+        Float max =x;
+        if(y.compareTo(max)>0)
+            max = y;
+//        if(z.floatValue()>max.floatValue())
+        if(y.compareTo(max)>0)
+            max= z;
+        return max;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Find Maximum problem using Generics");
--- src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
+++ src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
@@ -20,4 +20,19 @@ public class MaximumPrblmTest {
         Integer result = MaximumProblem.maximumNumber(4,6,8);
         Assert.assertEquals(8,result);
     }
+
+    @Test
+    public void toTest1stFloatIsMaximum() {
+        Float result = MaximumProblem.maximumNumber(4.5f,2.5f,0.5f);
+        Assert.assertEquals(4.5f,result);
+    }
+    @Test
+    public void toTest2stFloatIsMaximum() {
+        Float result = MaximumProblem.maximumNumber(3.5f,5.5f,4.5f);
+        Assert.assertEquals(5.5f,result);
+    }
+    @Test
+    public void toTest3stFloatIsMaximum() {
+        Float result = MaximumProblem.maximumNumber(4.5f,6.5f,8.5f);
+        Assert.assertEquals(8.5f,result);
 }
