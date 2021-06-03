01a8fd9fda942d9cf986da66991a872d862a7735
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-03 20:02:32 +0530

    Add-Three Integer to Find Maximum

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -1,6 +1,15 @@
 package com.bridgelabz.generics;
 
 public class MaximumProblem {
+    public static Integer maximumNumber(Integer x, Integer y, Integer z) {
+        Integer max =x;
+        if(y.compareTo(max)>0)
+            max =y;
+        if(z.compareTo(max)>0)
+            max =z;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Find Maximum problem using Generics");
     }
--- src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
+++ src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.genericstest;
 
+import com.bridgelabz.generics.MaximumProblem;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumPrblmTest {
+    @Test
+    public void toTest1stNumberIsMaximum() {
+        Integer result = MaximumProblem.maximumNumber(4,2,0);
+        Assert.assertEquals(4,result);
+    }
+    @Test
+    public void totest2ndNumberIsMaximum() {
+        Integer result = MaximumProblem.maximumNumber(3,5,4);
+        Assert.assertEquals(5,result);
+    }
+    @Test
+    public void totest3rdNumberIsMaximum() {
+        Integer result = MaximumProblem.maximumNumber(4,6,8);
+        Assert.assertEquals(8,result);
+    }
 }
