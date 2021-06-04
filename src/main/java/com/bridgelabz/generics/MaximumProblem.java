9db928f91e6afa1e58eb2e00ecabd1ad7725e3cf
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-04 09:23:45 +0530

    Refactor2- Generic class with Threevariables er

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -1,6 +1,22 @@
 package com.bridgelabz.generics;
 
-public class MaximumProblem {
+public class MaximumProblem <X,Y,Z extends Comparable>{
+    X xElement;
+    Y yElement;
+    Z zElement;
+
+    public MaximumProblem(X xElement, Y yElement, Z zElement) {
+        this.xElement = xElement;
+        this.yElement = yElement;
+        this.zElement = zElement;
+    }
+//    public void maximumNumber(X xElement, Y yElement, Z zElement){
+//        maximumNumber(xElement);
+//        maximumNumber(yElement);
+//        maximumNumber(zElement);
+//    }
+
+
     public static <E extends Comparable> E maximumNumber (E x, E y, E z){
         E max =x;
         if(y.compareTo(max)>0)
@@ -12,5 +28,6 @@ public class MaximumProblem {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Find Maximum problem using Generics");
+
     }
 }
--- src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
+++ src/test/java/com/bridgelabz/genericstest/MaximumPrblmTest.java
@@ -7,48 +7,57 @@ import org.junit.Test;
 public class MaximumPrblmTest {
     @Test
     public void toTest1stNumberIsMaximum() {
-        MaximumProblem.maximumNumber(4,2,0);
-        Assert.assertEquals(4,result);
+        MaximumProblem.maximumNumber(4, 2, 0);
+        Assert.assertEquals(4, result);
     }
+
     @Test
     public void totest2ndNumberIsMaximum() {
-        Integer result = MaximumProblem.maximumNumber(3,5,4);
-        Assert.assertEquals(5,result);
+        Integer result = MaximumProblem.maximumNumber(3, 5, 4);
+        Assert.assertEquals(5, result);
     }
+
     @Test
     public void totest3rdNumberIsMaximum() {
-        Integer result = MaximumProblem.maximumNumber(4,6,8);
-        Assert.assertEquals(8,result);
+        Integer result = MaximumProblem.maximumNumber(4, 6, 8);
+        Assert.assertEquals(8, result);
     }
+
     @Test
     public void toTest1stFloatIsMaximum() {
-        Float result = MaximumProblem.maximumNumber(4.5f,2.5f,0.5f);
-        Assert.assertEquals(4.5f,result);
+        Float result = MaximumProblem.maximumNumber(4.5f, 2.5f, 0.5f);
+        Assert.assertEquals(4.5f, result);
     }
+
     @Test
     public void toTest2stFloatIsMaximum() {
-        Float result = MaximumProblem.maximumNumber(3.5f,5.5f,4.5f);
-        Assert.assertEquals(5.5f,result);
+        Float result = MaximumProblem.maximumNumber(3.5f, 5.5f, 4.5f);
+        Assert.assertEquals(5.5f, result);
     }
+
     @Test
     public void toTest3stFloatIsMaximum() {
         Float result = MaximumProblem.maximumNumber(4.5f, 6.5f, 8.5f);
         Assert.assertEquals(8.5f, result);
     }
+
     @Test
     public void toTest1stStringIsMaximum() {
-        String result = MaximumProblem.maximumNumber("Banana","Apple","Peach");
-        Assert.assertEquals("Banana",result);
-    }
-    @Test
-    public void toTest2stStringIsMaximum() {
-        String result = MaximumProblem.maximumNumber("Apple","Banana","Peach");
-        Assert.assertEquals("Banana",result);
-    }
-    @Test
-    public void toTest3stStringIsMaximum() {
-        String result = MaximumProblem.maximumNumber("Apple","Peach","Banana");
-        Assert.assertEquals("Banana",result);
+        String result = MaximumProblem.maximumNumber("Banana", "Apple", "Peach");
+        Assert.assertEquals("Banana", result);
     }
 
+    @Test
+    public void toTest2stStringIsMaximum() {
+        String result = MaximumProblem.maximumNumber("Apple", "Banana", "Peach");
+        Assert.assertEquals("Banana", result);
+    }
+
+    @Test
+    public void toTest3stStringIsMaximum() {
+        String result = MaximumProblem.maximumNumber("Apple", "Peach", "Banana");
+        Assert.assertEquals("Banana", result);
+    }
 }
+
+
