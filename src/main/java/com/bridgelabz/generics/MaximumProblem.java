31d5e9fb6b00efb1c16aba64e0b4548e15f1ad6b
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-04 15:29:17 +0530

    Add-Generic Method to Print Maximum

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class MaximumProblem <E extends Comparable>{
-        E xElement;
-        E yElement;
-        E zElement;
-        public MaximumProblem(E xElement, E yElement, E zElement) {
+public class MaximumProblem <X extends Comparable>{
+        X xElement;
+        X yElement;
+        X zElement;
+        public MaximumProblem(X xElement, X yElement, X zElement) {
             this.xElement = xElement;
             this.yElement = yElement;
             this.zElement = zElement;
         }
 
-        public E testMaximum(){
+        public X testMaximum(){
         return maximumNumber(xElement,yElement,zElement);
     }
 
@@ -31,9 +31,18 @@ public class MaximumProblem <E extends Comparable>{
             List<E> sortElements = Arrays.stream(elements).sorted().collect(Collectors.toList());
             return sortElements.get(sortElements.size()-1);
         }
+        public static <T>void printmax(T x, T y, T z, T max){
+            System.out.println(x +" "+y+" "+z+" "+ max);
+        }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Find Maximum problem using Generics");
+        Integer xi=4, yi=9, zi=10;
+        Float xf=4.4f, yf=9.9f, zf=10.2f;
+        String xs="Apple",ys="Banana",zs="Peach";
+        new MaximumProblem<>(xi,yi,zi).testMaximum();
+        new MaximumProblem<>(xf,yf,zf).testMaximum();
+        new MaximumProblem<>(xs,ys,zs).testMaximum();
 
 
     }
